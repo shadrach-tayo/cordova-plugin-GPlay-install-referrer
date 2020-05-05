157d75707f82eb487d45046af5357cc3655ce5c6
shadrach <shadrachtemitayo@gmail.com>
2020-05-05 20:01:47 +0100

    feat: push final plugin code to test

--- www/googlePlayReferrer.js
+++ www/googlePlayReferrer.js
@@ -1,47 +1,25 @@
 var exec = require("cordova/exec");
 
-function getReferrer() {
+function getReferrer(success, error) {
+  if (!success) {
+    return new Promise(function (resolve, reject) {
+      getReferrer(resolve, reject);
+    });
+  }
+
   exec(
     function success(data) {
       console.log("success ", data);
+      if (ref) {
+        success(ref);
+      } else {
+        error("No referrer data");
+      }
     },
-    function error(reason) {
-      console.log("error ", reason);
-    },
+    error,
     "GooglePlayReferrer",
     "getReferrer"
   );
 }
 
 exports.getReferrer = getReferrer;
-
-// function GooglePlayReferrer() {}
-
-// // The function that passes work along to native shells
-// GooglePlayReferrer.prototype.getReferrer = function (
-//   successCallback,
-//   errorCallback
-// ) {
-//   console.log("get referrer plugin called: ");
-//   // if (!successCallback) {
-//   //   return new Promise((resolve, reject) => {
-//   //     this.getReferrer(resolve, reject);
-//   //   });
-//   // }
-
-//   exec(successCallback, errorCallback, "GooglePlayReferrer", "getReferrer", [
-//     {
-//       instance: "",
-//     },
-//   ]);
-// };
-
-// // Installation constructor that binds ToastyPlugin to window
-// GooglePlayReferrer.install = function () {
-//   if (!window.plugins) {
-//     window.plugins = {};
-//   }
-//   window.plugins.googlePlayReferrer = new GooglePlayReferrer();
-//   return window.plugins;
-// };
-// cordova.addConstructor(GooglePlayReferrer.install);
